570a80e2fdb4f25e63e5901577b63f725abf9929
steven0129 <h3584935@gmail.com>
2018-11-17 00:51:10 +0800

    Generate words using predict.py

--- predict.py
+++ predict.py
@@ -2,7 +2,15 @@ import sys
 import re
 from model import *
 from utils import *
-from tqdm import tqdm
+from gensim.models import FastText
+
+fastText = FastText.load_fasttext_format('wordvec/skipgram.bin')
+
+def word2vec(x):
+    try:
+        return torch.from_numpy(fastText.wv[x])
+    except:
+        return torch.rand(512)
 
 def load_model():
     src_vocab = load_vocab(sys.argv[2], "src")
@@ -26,7 +34,8 @@ def run_model(enc, dec, tgt_vocab, data):
         data.append(["", [EOS_IDX], []])
     data.sort(key = lambda x: len(x[1]), reverse = True)
     batch_len = len(data[0][1])
-    batch = LongTensor([x[1] + [PAD_IDX] * (batch_len - len(x[1])) for x in data])
+    batch = LongTensor([x[1] for x in data])
+    # batch = LongTensor([x[1] + [PAD_IDX] * (batch_len - len(x[1])) for x in data])
     mask = mask_pad(batch)
     enc_out = enc(batch, mask)
     # print(enc_out)
@@ -45,26 +54,42 @@ def run_model(enc, dec, tgt_vocab, data):
     for y in pred:
         for i in y:
             if i != PAD_IDX:
-                pass
-                # print(' '.join([tgt_vocab[i]]))
+                print(' '.join([tgt_vocab[i]]))
         # print(" ".join([tgt_vocab[i] for i in y if i != PAD_IDX]))
     return data[:z]
 
 def predict():
     data = []
+    pred = [[] for _ in range(BATCH_SIZE)]
     enc, dec, src_vocab, tgt_vocab = load_model()
-    fo = open(sys.argv[4])
-    for line in tqdm(fo):
-        line = line.strip()
-        x = tokenize(line, "word")
-        x = [src_vocab[i] for i in x if i != ' '] + [EOS_IDX]
-        data.append([line, x, []])
-        if len(data) == BATCH_SIZE:
-            result = run_model(enc, dec, tgt_vocab, data)
-            for x in result:
-                pass
-            data = []
-    fo.close()
+    with open(sys.argv[4]) as fo:
+        for line in fo.readlines():
+            line = line.strip()
+            x = list(filter(lambda x: x != ' ', tokenize(line, "word")))
+            mask = mask_pad(LongTensor([src_vocab[i] for i in x] + [EOS_IDX]))
+            x = torch.stack(list(map(word2vec, x + ['<EOS>'])))
+            enc_out = enc(x, mask)
+            dec_in = LongTensor([SOS_IDX] * BATCH_SIZE).unsqueeze(1)
+            
+            for t in range(1000):
+                dec_out = dec(enc_out, dec_in, mask)
+                dec_out = dec_out.long()
+                dec_in = torch.cat((dec_in, dec_out.data.topk(1)[1]), 1)
+                for i, j in enumerate(dec_out.data.topk(1)[1]):
+                    pred[i].append(scalar(j))
+
+            result = []
+            for y in pred:
+                for i in y:
+                    if i != PAD_IDX:
+                        result.append(tgt_vocab[i])
+
+            print(''.join(result))
+            # if len(data) == BATCH_SIZE:
+            #     result = run_model(enc, dec, tgt_vocab, data)
+            #     for x in result:
+            #         pass
+            #     data = []
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
